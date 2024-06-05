efa20aa563df990c9ac23fa7fa3de94fff1847bd
Jordan Harris <jordanharris1175@gmail.com>
2024-06-05 19:58:38 -0400

    Make layout changes to center content of home page
    and make footer absolute with a backdrop blur

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import { Separator } from "../ui/separator";
 
 export function Footer() {
   return (
-    <div className="flex items-center justify-center gap-2 py-2">
+    <footer className="container absolute bottom-0 left-0 right-0 z-50 flex items-center justify-center gap-2 bg-background/30 py-2 backdrop-blur-md">
       <a
         className="text-muted-foreground hover:text-foreground"
         href="https://github.com/JordanTHarris"
@@ -19,6 +19,6 @@ export function Footer() {
       >
         <Linkedin className="h-5 w-5" />
       </a>
-    </div>
+    </footer>
   );
 }
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,18 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
-import { ScrollArea } from "@/components/ui/scroll-area";
 
 export function Layout() {
   return (
     <div className="z-10 flex h-dvh max-h-dvh w-screen flex-col scrollbar-hide">
       <NavBar />
-      <ScrollArea className="mx-0 flex-1 overflow-y-auto overflow-x-hidden md:container md:mx-auto">
-        <Outlet />
-      </ScrollArea>
-      <footer className="container">
-        <Footer />
-      </footer>
+      <Outlet />
+      <Footer />
     </div>
   );
 }
