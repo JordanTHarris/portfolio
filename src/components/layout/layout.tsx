2a68dd3e8452e877fd1935fd1de683ecc761441e
Jordan Harris <jordanharris1175@gmail.com>
2024-06-12 17:27:46 -0400

    Use customized Magic UI dock in footer with link and tooltip

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,14 +2,17 @@ import { Outlet } from "react-router-dom";
 import { NavBar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
 import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "../ui/tooltip";
 
 export function Layout() {
   return (
     <div className="flex h-dvh max-h-dvh w-screen flex-col scrollbar-hide">
-      <NavBar />
-      <Outlet />
-      <Footer />
-      <Toaster richColors />
+      <TooltipProvider>
+        <NavBar />
+        <Outlet />
+        <Footer />
+        <Toaster richColors />
+      </TooltipProvider>
     </div>
   );
 }
