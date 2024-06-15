8182c3322c2d17c56803a7af3e7a69c2e617ef0b
Jordan Harris <jordanharris1175@gmail.com>
2024-06-15 14:24:50 -0400

    Change animation to not slide in from full bottom to prevent scroll

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -10,7 +10,7 @@ export function Projects() {
         words={[{ text: "<MyProjects/>", className: "font-mono" }]}
       />
 
-      <div className="mb-auto grid auto-cols-auto grid-cols-1 gap-6 px-5 duration-1000 animate-in fade-in slide-in-from-bottom md:grid-cols-2 xl:grid-cols-3">
+      <div className="mb-auto grid auto-cols-auto grid-cols-1 gap-6 px-5 duration-1000 animate-in fade-in slide-in-from-bottom-32 md:grid-cols-2 xl:grid-cols-3">
         <a href="https://github.com/JordanTHarris/note-app" target="_blank">
           <BackgroundGradient className="max-w-sm rounded-[22px] bg-secondary p-4 transition-[transform] duration-300 hover:scale-105">
             <img
