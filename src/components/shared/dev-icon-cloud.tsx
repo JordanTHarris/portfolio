d17d33dba9ba28b67c8aed23c8316639e48e2f43
Jordan Harris <jordanharris1175@gmail.com>
2024-06-15 18:55:14 -0400

    Add Tailwind to icon cloud and shuffle icon slugs on load

--- src/components/shared/dev-icon-cloud.tsx
+++ src/components/shared/dev-icon-cloud.tsx
@@ -1,31 +1,40 @@
 import { IconCloud } from "../ui/icon-cloud";
 
-const slugs = [
-  "typescript",
-  "javascript",
-  "java",
-  "react",
-  "html5",
-  "css3",
-  "nodedotjs",
-  "express",
-  "nextdotjs",
-  "prisma",
-  "postgresql",
-  "vercel",
-  "docker",
-  "git",
-  "github",
-  "visualstudiocode",
-  "shadcnui",
-  "remix",
-  "reactrouter",
-];
-
 export function DevIconCloud({ className }: { className?: string }) {
+  const slugs = [
+    "typescript",
+    "javascript",
+    "java",
+    "react",
+    "html5",
+    "css3",
+    "nodedotjs",
+    "express",
+    "nextdotjs",
+    "prisma",
+    "postgresql",
+    "vercel",
+    "docker",
+    "git",
+    "github",
+    "visualstudiocode",
+    "shadcnui",
+    "remix",
+    "reactrouter",
+    "tailwindcss",
+  ];
+
+  function shuffle(array: string[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
   return (
     <div className={className}>
-      <IconCloud className="" iconSlugs={slugs} />
+      <IconCloud className="" iconSlugs={shuffle(slugs)} />
     </div>
   );
 }
