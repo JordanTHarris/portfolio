e663c24120b53e458b5efd1ae441915a9db7be70
Jordan Harris <jordanharris1175@gmail.com>
2024-06-15 17:48:01 -0400

    Use tailwind-scrollbar plugin to style scrollbars across browsers

--- tailwind.config.js
+++ tailwind.config.js
@@ -82,6 +82,10 @@ module.exports = {
   },
   plugins: [
     require("tailwindcss-animate"),
+    require("tailwind-scrollbar")({
+      nocompatible: true,
+      preferredStrategy: "pseudoelements",
+    }),
     plugin(function ({ addUtilities }) {
       addUtilities({
         ".scrollbar-hide": {
